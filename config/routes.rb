3834e807059bfeb8b8908db876076a0ca7845505
matcho3 <matcho3@kohama-no-MacBook-Air.local>
2014-11-27 19:25:49 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get "about/index"
+  devise_for :users
+  root "about#index"
+  
+end

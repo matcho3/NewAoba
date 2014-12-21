f3715e279a3b3331d3b194fd09b990ea277aa7d0
matcho3 <matcho3@kohama-no-MacBook-Air.local>
2014-12-21 22:05:30 +0900

    fixed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 	def mail_send
     @mail = NoticeMailer.sendmail_confirm.deliver
-    render :text => 'send finish'
+    # render :text => '送信完了しました。'
     # raise 'hi'
   end
 end

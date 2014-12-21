3e32c8f8f01d40aef187696ef5cdcb78bcbc5aa8
matcho3 <matcho3@kohama-no-MacBook-Air.local>
2014-12-22 01:44:30 +0900

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   # match '/sendmail',to:'items#mail_send',via:'get'
    # match '/confirm/:id',to:'users#confirm', as:'user_confirm', via:'get'
   # match '/answer',to:'answers#answer',via:'get'
-  match '/sendmail/:id',to:'items#mail_send',as:'sendmail',via:'get'
+  match '/sendmail/',to:'items#mail_send',as:'sendmail',via:'get'
 # post '/answers/save',   to: 'answers#save',  as: :answer_save
 
   resources :answers

b0288871d375e23ec14f9ecedf57479fab65abd6
matcho3 <matcho3@kohama-no-MacBook-Air.local>
2015-01-08 03:50:27 +0900

    almost

--- app/assets/javascripts/answer.js
+++ app/assets/javascripts/answer.js
@@ -1,3 +1,5 @@
+
+
 $(function() {
 	$('#btn').click(function(event) {
 
@@ -6,32 +8,46 @@ $(function() {
 	 	var form = $('form');
 
 		var val = $('form :checked').val();
-        console.log(val); 
-	 	for(var i=0; i<form.length; i++){
+        console.log(val);
+        var isAllFilled = true;
+	 	for(var i=0; i<form.length; i++)　{
 	 		var $form = form.eq(i);
-		console.log($form.serialize());
-	 	// var  = $form.serialize();
-	 	$.ajax({
-	 		url: $form.attr('action'),
-	 		type: $form.attr('method'),
-	 		data: $form.serialize(),
-            // timeout: 10000,
-            // dataType: 'text'
-        })
-	 	.success(function( data ) {
-            // 通信が成功したときの処理
-            console.log('success');
-        })
-	 	.error(function( data ) {
-            // 通信が失敗したときの処理
-            console.log('error');
-        })}
+			console.log($form.serialize());
+
+			if() {//　もし入っていなかったら
+	 			isAllFilled = false;
+	 			// TODO: バリーションメッセージを表示する
+	 			$('.error-message').html('入力されてないよ');
+	 		}
+		}
+		if(isAllFilled == true) { // 全部のフォームが入力されていたら
+			for(var i=0; i<form.length; i++) {
+				var $form = form.eq(i);
+				$.ajax({
+			 		url: $form.attr('action'),
+			 		type: $form.attr('method'),
+			 		data: $form.serialize(),
+		            // timeout: 10000,
+		            // dataType: 'text'
+		        })
+			 	.success(function( data ) {
+		            // 通信が成功したときの処理
+		            console.log('success');
+
+		            location.href = '/ope';
+		        })
+			 	.error(function( data ) {
+		            // 通信が失敗したときの処理
+		            console.log('error');
+		        });
+		    }
+	    } else {
+	    	#('.error-message').show();
+	    }
 	 });
 });
 
-		jQuery(document).ready(function(){
-             jQuery('#item3t').validationEngine();
-          });
+		
 
 // 最終的に、answer controller 送って保存させる。
 // answer の、answer actionに行くように保存させる
